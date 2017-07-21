f5dd58ae633e9180b5927e1cbe90ce1a14ecd6c7
Sven Koschnicke <s.koschnicke@gfxpro.com>
2017-07-21 11:54:55 +0200

    player

--- lib/software_challenge_client/player.rb
+++ lib/software_challenge_client/player.rb
@@ -1,59 +1,31 @@
 # encoding: UTF-8
 
-# A player, participating in a game
+# Ein Spieler
 class Player
   # @!attribute [r] name
-  # @return [String] the player's name
+  # @return [String] der Name des Spielers, hat keine Auswirkungen auf das Spiel
   attr_reader :name
 
   # @!attribute [r] color
-  # @return [PlayerColor] the player's color
+  # @return [PlayerColor] die Farbe des Spielers, Rot oder Blau
   attr_reader :color
 
   # @!attribute [rw] points
-  # @return [Integer] the player's points
+  # @return [Integer] der aktuelle Punktestand des Spielers
   attr_accessor :points
 
-  # @!attribute [rw] velocity
-  # @return [Integer] the player's current velocity
-  attr_accessor :velocity
+  # @!attribute [rw] index
+  # @return [Integer] die aktuelle Position des Spielers auf dem Spielbrett, entspricht index des Feldes, von 0 bis 64
+  attr_accessor :index
 
-  # @!attribute [rw] coal
-  # @return [Integer] the player's current coal supply
-  attr_accessor :coal
-
-  # @!attribute [rw] direction
-  # @return [Direction] the player's current direction
-  attr_accessor :direction
-
-  # @!attribute [rw] x
-  # @return [Integer] the player's current x-position
-  attr_accessor :x
-
-  # @!attribute [rw] y
-  # @return [Integer] the player's current y-position
-  attr_accessor :y
-
-  # @!attribute [rw] movement
-  # @return [Direction] the player's current movement points
-  attr_accessor :movement
-
-  # @!attribute [rw] passengers
-  # @return [Integer] how many passengers the player's has picked up
-  attr_accessor :passengers
-
-  # Initializer
-  # @param color [PlayerColor] the new player's color
-  # @param name [String] the new player's name (for displaying)
+  # Konstruktor
+  # @param color [PlayerColor] Farbe
+  # @param name [String] Name
   def initialize(color, name)
     @color = color
     @name = name
     @points = 0
-    @velocity = 1
-    @movement = 1
-    @coal = 6
-    @passengers = 0
-    @direction = Direction::RIGHT
+    @index = 0
   end
 
   def ==(other)
