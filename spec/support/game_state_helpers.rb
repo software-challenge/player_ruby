82a2a091592305e7a331aa88b66371373252ba76
Sven Koschnicke <s.koschnicke@gfxpro.com>
2019-10-28 22:00:15 +0100

    chore: formatting

--- spec/support/game_state_helpers.rb
+++ spec/support/game_state_helpers.rb
@@ -1,32 +1,46 @@
+# frozen_string_literal: true
+
 require_relative '../../lib/software_challenge_client/util/constants'
 
 module GameStateHelpers
-
-  class BoardFormatError < StandardError;
+  class BoardFormatError < StandardError
   end
 
   def state_from_string!(string, gamestate)
-    board = Board.new
     fields = Board.new.field_list.sort do |a, b|
       cmp_z = a.coordinates.z <=> b.coordinates.z
       cmp_z == 0 ? a.coordinates.x <=> b.coordinates.x : cmp_z
-    end.map{ |f| {x: f.coordinates.x, y: f.coordinates.y} }
-    fieldDescriptors = string.gsub(/\s/, '')
-    boardFields = []
+    end.map { |f| { x: f.coordinates.x, y: f.coordinates.y } }
+    field_descriptors = string.gsub(/\s/, '')
+    board_fields = []
     fields.each_with_index do |c, i|
-      boardFields << case fieldDescriptors[i * 2]
-                     when 'R'
-                       Field.new(c[:x], c[:y], [Piece.new(PlayerColor::RED, PieceType.find_by_value(fieldDescriptors[i * 2 + 1]))])
-                     when 'B'
-                       Field.new(c[:x], c[:y], [Piece.new(PlayerColor::BLUE, PieceType.find_by_value(fieldDescriptors[i * 2 + 1]))])
-                     when 'O'
-                       Field.new(c[:x], c[:y], [], true)
-                     when '-'
-                       Field.new(c[:x], c[:y])
-                     else
-                       throw BoardFormatError.new("Unknown field type")
-                     end
+      board_fields << case field_descriptors[i * 2]
+                      when 'R'
+                        Field.new(
+                          c[:x],
+                          c[:y],
+                          [Piece.new(PlayerColor::RED,
+                                     PieceType.find_by_value(
+                                       field_descriptors[i * 2 + 1]
+                                     ))]
+                        )
+                      when 'B'
+                        Field.new(
+                          c[:x],
+                          c[:y],
+                          [Piece.new(PlayerColor::BLUE,
+                                     PieceType.find_by_value(
+                                       field_descriptors[i * 2 + 1]
+                                     ))]
+                        )
+                      when 'O'
+                        Field.new(c[:x], c[:y], [], true)
+                      when '-'
+                        Field.new(c[:x], c[:y])
+                      else
+                        throw BoardFormatError.new('Unknown field type')
+                      end
     end
-    gamestate.board = Board.new(boardFields)
+    gamestate.board = Board.new(board_fields)
   end
 end
