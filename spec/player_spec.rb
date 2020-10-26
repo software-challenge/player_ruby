8ddbf759692e8862795e57dd2325cc7d3686407a
niklasCarstensen <niklas.carstensen@gmx.de>
2020-10-26 14:42:54 +0100

    Update player spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,12 +4,12 @@
 # Read http://betterspecs.org/ for suggestions writing good specs.
 
 RSpec.describe Player do
-  subject(:player) { Player.new(PlayerColor::RED, 'name') }
+  subject(:player) { Player.new(PlayerType::TWO, 'uwu') }
 
   context 'when newly created' do
     it 'should have a color and a name' do
-      expect(player.color).to eq(PlayerColor::RED)
-      expect(player.name).to eq('name')
+      expect(player.type).to eq(PlayerType::TWO)
+      expect(player.name).to eq('uwu')
     end
   end
 end
