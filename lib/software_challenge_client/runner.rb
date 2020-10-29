9e042eecbd86d59c3b71b47ec902663f5564dddf
niklasCarstensen <niklas.carstensen@gmx.de>
2020-10-29 19:35:09 +0100

    Update sc year

--- lib/software_challenge_client/runner.rb
+++ lib/software_challenge_client/runner.rb
@@ -9,7 +9,7 @@ class Runner
   include Logging
 
   def initialize(host, port, client, reservation = nil)
-    logger.info 'Software Challenge 2019'
+    logger.info 'Software Challenge 2021'
     logger.info 'Ruby Client'
     logger.info "Host: #{host}"
     logger.info "Port: #{port}"
